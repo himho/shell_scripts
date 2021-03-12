848e372bcd97d645a61c0ca240a2980e980ba641
himho <vioze.lee@gmail.com>
2021-03-12 14:54:31 +0800

    Update shell_script_mod.sh

--- shell_script_mod.sh
+++ shell_script_mod.sh
@@ -13,8 +13,9 @@ else
     git -C /shylocks reset --hard
     git -C /shylocks pull --rebase
     npm install --loglevel error
-fi
+	fi
 ##复制外挂配置脚本
+cat /scripts/docker/crontab_list
 cp -f /shylocks/shell_script_mod.sh /scripts
 ##复制脚本文件
 cp -f /shylocks/jd* /shylocks/sq* /scripts/
