452d2e673593dfe9aff0e793eebacf756f9ab2ba
himho <43086765+himho@users.noreply.github.com>
2021-03-14 09:08:58 +0800

    Update crontab_list.sh

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -2,4 +2,4 @@
 48 */1 * * * sh /scripts/docker_entrypoint2.sh |ts >> /scripts/logs/default_task.log 2>&1
 50 23 */3 * * rm /scripts/logs/*.log
 15 8,12 * * * node /scripts/sqxs.js >> /scripts/logs/sqxs.log 2>&1
-15,45 0-23 * * * node /scripts/jd_bububao.js >> /scripts/logs/jd_bububao.log 2>&1
+15,45 0-23 * * * node /scripts/bububao.js >> /scripts/logs/bububao.log 2>&1
