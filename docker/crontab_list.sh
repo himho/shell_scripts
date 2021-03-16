c151036cca664a9c14339a34362391b9c36e2718
himho <43086765+himho@users.noreply.github.com>
2021-03-16 09:46:23 +0800

    Update crontab_list.sh

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -3,3 +3,4 @@
 50 23 */2 * * rm /scripts/logs/*.log
 15 8,12 * * * node /scripts/sqxs.js >> /scripts/logs/sqxs.log 2>&1
 15,45 0-23 * * * node /scripts/bububao.js >> /scripts/logs/bububao.log 2>&1
+12,42 8-20 * * * node /scripts/zfz.js >> /scripts/cfz.log 2>&1
